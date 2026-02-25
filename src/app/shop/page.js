050bf989322cfcd17ece1b247be06a76172df220
MD SAMIUL HASAN <samiularfinmd@gmail.com>
2026-02-25 10:24:59 +0600

    Some Update

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -593,17 +593,42 @@ export default function ShopPage() {
             </button>
 
             {/* Search */}
-            <div className="flex-1 relative justify-center">
-              <Search size={17} className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 dark:text-gray-900 pointer-events-none" />
+            // <div className="flex-1 relative justify-center">
+            //   <Search size={17} className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 dark:text-gray-900 pointer-events-none" />
+            //   <input
+            //     type="text"
+            //     placeholder="Search products..."
+            //     value={searchQuery}
+            //     onChange={(e) => setSearchQuery(e.target.value)}
+            //     className="w-auto pl-9 pr-4 py-2 rounded-full border border-gray-200 bg-gray-50 text-sm text-gray-900 focus:outline-none focus:border-[#FF5533] focus:bg-white transition-colors"
+            //   />
+            //   {searchQuery && (
+            //     <button onClick={() => setSearchQuery('')} className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 cursor-pointer">
+            //       <X size={16} />
+            //     </button>
+            //   )}
+            // </div>
+
+            <div className="relative w-full max-w-md">
+              {/* Search Icon */}
+              <Search
+                size={17}
+                className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none"
+              />
+              {/* Input */}
               <input
                 type="text"
                 placeholder="Search products..."
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
-                className="w-auto pl-9 pr-4 py-2 rounded-full border border-gray-200 bg-gray-50 text-sm text-gray-900 focus:outline-none focus:border-[#FF5533] focus:bg-white transition-colors"
+                className="w-full pl-9 pr-9 py-2 rounded-full border border-gray-200 bg-gray-50 text-sm text-gray-900 focus:outline-none focus:border-[#FF5533] focus:bg-white"
               />
+              {/* Cross Icon */}
               {searchQuery && (
-                <button onClick={() => setSearchQuery('')} className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 cursor-pointer">
+                <button
+                  onClick={() => setSearchQuery('')}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
+                >
                   <X size={16} />
                 </button>
               )}
